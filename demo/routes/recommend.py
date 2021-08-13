e6a2bc48d33384b393bb7317b32deb3793410ae6
Zhaolun Shi <zxs172030@utdallas.edu>
2021-08-13 15:38:57 -0500

    Update recommend.py

--- demo/routes/recommend.py
+++ demo/routes/recommend.py
@@ -34,14 +34,14 @@ def login_required(func):
 
 
 def getSimModle():
-    # 默认载入movielens数据集
+    # add data
     data = Dataset.load_builtin('ml-100k')
     trainset = data.build_full_trainset()
-    #使用pearson_baseline方式计算相似度  False以item为基准计算相似度 本例为电影之间的相似度
+    # pearson_baseline
     sim_options = {'name': 'pearson_baseline', 'user_based': False}
-    ##使用KNNBaseline算法
+    ## KNNBaseline to compute
     algo = KNNBaseline(sim_options=sim_options)
-    #训练模型
+    # train model
     algo.fit(trainset)
     return algo
 
@@ -49,7 +49,7 @@ def getSimModle():
 
 def read_item_names():
     """
-    获取电影名到电影id 和 电影id到电影名的映射
+    
     """
     file_name = (os.path.expanduser('~') +
                  '/.surprise_data/ml-100k/ml-100k/u.item')
@@ -64,21 +64,21 @@ def read_item_names():
 
 
 def showSimilarMovies(algo, user_id):
-    # 获得电影Toy Story (1995)的raw_id
+    
     toy_story_raw_id = str(user_id)
     print("+++++++"+toy_story_raw_id)
     logging.debug('raw_id=' + toy_story_raw_id)
-    #把电影的raw_id转换为模型的内部id
+    # useless now
     toy_story_inner_id = algo.trainset.to_inner_iid(toy_story_raw_id)
     logging.debug('inner_id=' + str(toy_story_inner_id))
-    #通过模型获取推荐电影 这里设置的是10部
+    # get 10 best neighbors
     toy_story_neighbors = algo.get_neighbors(toy_story_inner_id, 10)
     logging.debug('neighbors_ids=' + str(toy_story_neighbors))
-    #模型内部id转换为实际电影id
+    # useless now
     neighbors_raw_ids = [algo.trainset.to_raw_iid(inner_id) for inner_id in toy_story_neighbors]
-    #通过电影id列表 或得电影推荐列表
+    # return the 
     # neighbors_movies = [rid_to_name[raw_id] for raw_id in neighbors_raw_ids]
-    print('The 10 nearest neighbors of user are:')
+    # print('The 4 nearest neighbors of user are:')
     return neighbors_raw_ids[:4]
 
 # rid_to_name, name_to_rid = read_item_names()
